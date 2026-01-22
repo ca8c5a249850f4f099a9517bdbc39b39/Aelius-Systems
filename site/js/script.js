15770fdd786a3ef1b2bddb33790212224802ac86
ca8c5a249850f4f099a9517bdbc39b39 <ca8c5a249850f4f099a9517bdbc39b39@proton.me>
2026-01-21 22:44:55 -0500

    Delete site/js directory

--- site/js/script.js
+++
@@ -1,9 +0,0 @@
-const scrollContainer = document.getElementById("scrollContainer");
-
-document.querySelector(".left-btn").addEventListener("click", () => {
-    scrollContainer.scrollLeft -= 200;
-});
-
-document.querySelector(".right-btn").addEventListener("click", () => {
-    scrollContainer.scrollLeft += 200;
-});
